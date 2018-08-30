bc667331aafc2e492e968900c6f48eaf809705e3
Tyler Moore <mooretc2@miamioh.edu>
2018-08-30 15:15:57 -0700

    First content rendering

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. This component should produce some html
+
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+// Take this component's generated html and put it on the page
+// (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
