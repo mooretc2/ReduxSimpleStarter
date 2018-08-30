7e98ce6c379eecb472180a6c34e3f8f825271254
Tyler Moore <mooretc2@miamioh.edu>
2018-08-30 15:47:21 -0700

    Several new files, now renders an input as a nested component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+const API_KEY = process.env.REACT_APP_YT_KEY;
 
 // Create a new component. This component should produce some html
-
 const App = () => {
-    return <div>Hi!</div>;
+    return <div>
+        <SearchBar />
+    </div>;
 }
 
 // Take this component's generated html and put it on the page
 // (in the DOM)
-
 ReactDOM.render(<App />, document.querySelector('.container'));
