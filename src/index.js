9ab84ae7bd4ea1228059fae2870dd806d6c6cdc6
Tyler Moore <mooretc2@miamioh.edu>
2018-09-02 21:39:27 -0700

    Video list functional

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-const API_KEY = process.env.REACT_APP_YT_KEY;
+import VideoList from './components/video_list';
+// If you really want to steal this, go for it I guess?
+// I'll be deleting it in a week or so anyway
+const API_KEY = 'AIzaSyANQJsaU8X63UVi1MZ2v7688WIrjcJDcKY';
+
+
 
 // Create a new component. This component should produce some html
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>;
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated html and put it on the page
